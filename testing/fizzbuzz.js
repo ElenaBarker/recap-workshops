0e0343aa0c9936a1c31c603360fb721e42ee83be
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-05-02 13:41:10 +0100

    Fizzbuzz updated

--- testing/fizzbuzz.js
+++ testing/fizzbuzz.js
@@ -1,3 +1,10 @@
-function fizzbuzz(number) {}
+function fizzbuzz(number) {
+    const numbers = [];
+    for (let i = 1; i <= number; i++){
+    numbers.push(i);
+    }
+return numbers.join();
+    
+}
 
 export default fizzbuzz;
